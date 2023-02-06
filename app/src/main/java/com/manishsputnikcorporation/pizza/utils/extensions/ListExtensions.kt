0014ae4f2624b3b22d96688a0f7ca1309aff2589
Manuel Espinosa Torres <maneletorres@hotmail.com>
2023-02-06 20:14:32 +0100

    ViewModel tests fixed.

--- app/src/main/java/com/manishsputnikcorporation/pizza/utils/extensions/ListExtensions.kt
+++ app/src/main/java/com/manishsputnikcorporation/pizza/utils/extensions/ListExtensions.kt
@@ -1,7 +1,7 @@
 package com.manishsputnikcorporation.pizza.utils.extensions
 
+import android.content.Context
 import android.content.res.Resources
-import androidx.fragment.app.Fragment
 import com.manishsputnikcorporation.pizza.R
 import com.manishsputnikcorporation.pizza.domain.Pizza
 
@@ -31,10 +31,10 @@ fun List<Pizza>.toFormattedPizzaList(resources: Resources): String =
     }
 
 // TODO: enhance this method with default function extensions.
-fun List<Int>.toPizzaList(fragment: Fragment, quantity: Int): MutableList<Pizza> {
+fun List<Int>.toPizzaList(context: Context, quantity: Int): MutableList<Pizza> {
     val pizzas = mutableListOf<Pizza>()
     forEachIndexed { index, pizzaResourceId ->
-        pizzas.add(Pizza(fragment.getString(pizzaResourceId), if (index == 0) quantity else 0))
+        pizzas.add(Pizza(context.getString(pizzaResourceId), if (index == 0) quantity else 0))
     }
     return pizzas
 }
